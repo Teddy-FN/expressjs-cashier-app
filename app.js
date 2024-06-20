d03ef260fa4b0661ef0aa41fe4295b2beeb428da
Teddy-FN <tedd.ferdy@gmail.com>
2024-06-20 16:03:32 +0700

    Ui Login

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+const path = require("path");
 
 const adminRoutes = require("./routes/admin");
 
 // Error Controller
 const errorController = require("./controller/error");
 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+// Login Page
+app.use("/", (req, res, next) => {
+  res.render("login.ejs");
+});
+
+// Admin Routes Page
+app.use("/admin", adminRoutes);
+
 // Error Page
-app.use(adminRoutes);
 app.use(errorController.error);
 
 app.listen(5000);
