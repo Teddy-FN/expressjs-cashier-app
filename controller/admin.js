82ac827d3fff156d0f08f89b5e8997cee02c519c
Teddy-FN <tedd.ferdy@gmail.com>
2024-06-21 16:56:00 +0700

    Add Feature Edit Product

--- controller/admin.js
+++ controller/admin.js
@@ -2,71 +2,71 @@ let product = [
   {
     id: 1,
     img: "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    category: "food",
-    productName: "Makanan",
+    category: "Makanan",
+    productName: "Nasgor",
     price: "20.000",
   },
   {
     id: 2,
     img: "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    category: "food",
-    productName: "Makanan",
+    category: "Makanan",
+    productName: "Bihun",
     price: "20.000",
   },
   {
     id: 3,
     img: "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    category: "food",
-    productName: "Makanan",
+    category: "Makanan",
+    productName: "Capcay",
     price: "20.000",
   },
   {
     id: 4,
     img: "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    category: "food",
-    productName: "Makanan",
+    category: "Makanan",
+    productName: "Soto",
     price: "20.000",
   },
   {
     id: 5,
     img: "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    category: "food",
-    productName: "Makanan",
+    category: "Makanan",
+    productName: "Bakso",
     price: "20.000",
   },
   {
     id: 6,
     img: "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    category: "food",
-    productName: "Makanan",
+    category: "Makanan",
+    productName: "Miso",
     price: "20.000",
   },
   {
     id: 7,
     img: "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    category: "food",
-    productName: "Makanan",
+    category: "Makanan",
+    productName: "Mie Gacoan",
     price: "20.000",
   },
   {
     id: 8,
     img: "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    category: "food",
-    productName: "Makanan",
+    category: "Makanan",
+    productName: "Indomie",
     price: "20.000",
   },
   {
     id: 9,
     img: "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    category: "food",
-    productName: "Makanan",
+    category: "Makanan",
+    productName: "Paklay",
     price: "20.000",
   },
   {
     id: 10,
     img: "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    category: "food",
-    productName: "Makanan",
+    category: "Makanan",
+    productName: "Batagor",
     price: "20.000",
   },
 ];
@@ -86,6 +86,13 @@ exports.renderFormAdd = (req, res, next) => {
     pageTitle: "Add Product",
     admin: true,
     url: req.protocol + "://" + req.header.host,
+    item: {
+      id: null,
+      img: null,
+      category: null,
+      productName: null,
+      price: null,
+    },
   });
 };
 
@@ -102,8 +109,43 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 // Render Edit Form Product
+exports.renderFormEdit = (req, res, next) => {
+  const getProduct = product.filter(
+    (items) => items.id === Number(req.params.id)
+  );
+  const [prouduct] = getProduct;
+
+  res.render("admin/formProduct.ejs", {
+    pageTitle: "Edit Product",
+    admin: true,
+    url: req.protocol + "://" + req.header.host,
+    item: {
+      id: prouduct.id,
+      img: prouduct.img,
+      category: prouduct.category,
+      product: prouduct.productName,
+      price: prouduct.price,
+    },
+  });
+};
 
 // Function Put Edit Form Product
+exports.EditProduct = (req, res, next) => {
+  const getIndexProduct = product.findIndex(
+    (items) => items.id === Number(req.params.id)
+  );
+  console.log("getIndexProduct =>", getIndexProduct);
+
+  product[getIndexProduct] = {
+    id: req.params.id,
+    img: req.body.image,
+    category: req.body.category,
+    productName: req.body.product,
+    price: req.body.price,
+  };
+
+  res.redirect("/admin/list");
+};
 
 // Delete
 exports.deleteProduct = (req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ const adminController = require("../controller/admin");
 
 router.get("/list", adminController.home);
 
-// Render Form
+// Render Form Add Product
 router.get("/add-product", adminController.renderFormAdd);
 
 // Function Post Product
@@ -15,5 +15,11 @@ router.post("/add-product", adminController.postAddProduct);
 
 // Function Delete
 router.post("/delete-product", adminController.deleteProduct);
+
+// Render Form Edit Product
+router.get("/edit-product/:id", adminController.renderFormEdit);
+
+// Function Post Edit Product
+router.post("/edit-product/:id", adminController.EditProduct);
 
 module.exports = router;
