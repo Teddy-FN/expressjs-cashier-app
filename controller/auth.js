f0429fa9d0ea98cac53808f93d75368d44f33f5b
Teddy-FN <tedd.ferdy@gmail.com>
2024-07-06 18:00:54 +0700

    Register User

--- controller/auth.js
+++ controller/auth.js
@@ -32,6 +32,56 @@ exports.login = (req, res, next) => {
   );
 };
 
+// Register
+exports.registerNewUser = async (req, res, next) => {
+  const { username, password, confirmationPassword } = req.body;
+
+  if (password !== confirmationPassword) {
+    res.render("register.ejs", {
+      pageTitle: "Register New Account",
+      url: req.protocol + "://" + req.header.host,
+      error: "Password & Konfirmasi password tidak sama",
+      success: false,
+    });
+  } else {
+    return db.pool.query(
+      'SELECT * FROM public."User" ORDER BY id ASC',
+      [],
+      (err, response) => {
+        console.log("RESPONSE =>", response);
+        const checkUser = response.rows.filter(
+          (items) => items.userName === username
+        )[0];
+
+        if (checkUser) {
+          res.render("register.ejs", {
+            pageTitle: "Register New Account",
+            url: req.protocol + "://" + req.header.host,
+            error: "User sudah tersedia",
+            success: false,
+          });
+        } else {
+          console.log("cokk");
+          return db.pool.query(
+            'INSERT INTO public."User"("userName", password, role) VALUES ($1, $2, $3)',
+            [username, password, "user"],
+            (err, response) => {
+              console.log("response response =>", response);
+              res.render("register.ejs", {
+                pageTitle: "Register New Account",
+                url: req.protocol + "://" + req.header.host,
+                error: "",
+                success: true,
+              });
+            }
+          );
+        }
+      }
+    );
+  }
+};
+
+// User Logout
 exports.logout = (req, res, next) => {
   res.clearCookie("userName");
   res.clearCookie("password");
