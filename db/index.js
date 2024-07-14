08c4cd49e404238e8ca5ce354ac4ec267f545e27
Teddy-FN <tedd.ferdy@gmail.com>
2024-07-14 12:26:09 +0700

    vercel-push

--- db/index.js
+++ db/index.js
@@ -1,12 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.USERPOSTGREE,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: parseInt(process.env.PORT || "5432"),
-  host: process.env.HOST,
-  connectionTimeoutMillis: 200,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 module.exports = {
